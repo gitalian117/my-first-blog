18c747038c5342369c88c7ab4003c7ad134dded2
Ryota Yasuda <gitalian117@gmail.com>
2020-06-10 17:38:13 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-
+from .forms import PostForm
+from django.shortcuts import redirect
 # Create your views here.
 
 def post_list(request):
@@ -12,3 +13,54 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+    
+
+"""
+Postはクラス、requestとpkに対応したインスタンス＝投稿を返す。pkはurlsの中で定義されていて、このviewsを参照するように定義つけてれている。
+1個目のifの意味はリクエストに対して要求されてるのがPOST関数の場合、つまりなんらかの編集を加えて”送信している場合”にこっちの条件分岐に飛ぶってこと。
+2個目のifは純粋にエラーじゃないかを確認してる（投稿の値が正常か。（空のまま送信してきたりしてないかとか））
+で、elseは送信とかじゃない場合。
+ちなみに
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = {'title', 'text',}
+ていうクラス。
+で、ModelFormはその名の通りモデルからフォームを作成する。つまり本来フォームを作るときにはフィールドの定義が必要やけど、すでに入れたいモデルが定義されてる場合、それをそのまま入れましょうっていう関数。今回は下につけてモデルはPostって入れてる。
+
+"""
+
+
+
+
+    
